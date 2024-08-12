eb3dacd25fe0c482b299bbafadc6a78af0ffb792
alittleboy <1304592019@qq.com>
2024-08-12 18:55:11 +0800

    Update love.js

--- dependence/love.js
+++ dependence/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "17px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("芬芬点这", 23, 16);
+            ctx.fillText("藤藤点这", 23, 16);
             ctx.restore();
         },
         clear: function() {
